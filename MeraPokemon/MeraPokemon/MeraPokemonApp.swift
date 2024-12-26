4f308e2f062800bc9eb1d1c797fcd226d981727c
MinhTego <nn.minh@tego.global>
2024-12-26 10:56:12 +0700

    init project

---
+++ MeraPokemon/MeraPokemon/MeraPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeraPokemonApp.swift
+//  MeraPokemon
+//
+//  Created by Nhat Minh on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MeraPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
